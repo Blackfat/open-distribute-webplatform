0886c1957e3e37be33443d3897bdb414d477a0e7
wangfeiyang <wangfeiyang@huaxiafinance.com>
2017-06-21 15:42:47 +0800

    web-open初始化

---
+++ platform-web-open/src/main/java/com/blackfat/open/platform/common/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.blackfat.open.platform.common;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.models.dto.ApiInfo;
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+
+/**
+ * SwaggerConfig 总容器配置类
+ */
+@Configuration
+@EnableSwagger
+public class SwaggerConfig {
+
+	private SpringSwaggerConfig springSwaggerConfig;
+
+	/**
+	 * Required to autowire SpringSwaggerConfig
+	 */
+	@Autowired
+	public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
+		this.springSwaggerConfig = springSwaggerConfig;
+	}
+
+	/**
+	 * Every SwaggerSpringMvcPlugin bean is picked up by the swagger-mvc
+	 * framework - allowing for multiple swagger groups i.e. same code base
+	 * multiple swagger resource listings.
+	 */
+	@Bean
+	public SwaggerSpringMvcPlugin customImplementation() {
+		return new SwaggerSpringMvcPlugin(this.springSwaggerConfig).apiInfo(apiInfo()).includePatterns(".*?");
+	}
+
+	private ApiInfo apiInfo() {
+		ApiInfo apiInfo = new ApiInfo("提供wap及app开发接口","<h4>提供html5、app平台api</h4>", "", "", "", "");
+		return apiInfo;
+	}
+}
