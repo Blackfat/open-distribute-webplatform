b3d1b3748b7dfbc9578d021b314b30f6c24119d3
Blackfat <15715556946@163.com>
2022-07-23 20:54:53 +0800

    learn agent

---
+++ platform-javaagent-learn/src/main/java/com/blackfat/learn/agent/MyTest.java
@@ -0,0 +1,27 @@
+/**
+ * ymm56.com Inc.
+ * Copyright (c) 2013-2022 All Rights Reserved.
+ */
+package com.blackfat.learn.agent;
+
+/**
+ *
+ * @author feiyang.wang
+ * @version MyTest.java, v 0.1 2022-07-22 10:24 Exp $$
+ */
+public class MyTest {
+
+    public static void main(String[] args) {
+        new MyTest().foo();
+    }
+    public void foo() {
+        bar1();
+        bar2();
+    }
+
+    public void bar1() {
+    }
+
+    public void bar2() {
+    }
+}
